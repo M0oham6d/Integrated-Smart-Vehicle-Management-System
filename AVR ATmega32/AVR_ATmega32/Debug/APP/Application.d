bc2032f0238c2ab5872222f452492e80e96acbaf
M0oham6d <mo321343@gmail.com>
2025-02-12 17:06:50 +0200

    AVR ATmega32 Done

---
+++ AVR ATmega32/AVR_ATmega32/Debug/APP/Application.d
@@ -0,0 +1,62 @@
+APP/Application.o APP/Application.o: ../APP/Application.c \
+  ../APP/Application.h ../APP/../HAL/PIR/pir.h \
+  ../APP/../HAL/PIR/../../MCAL/GPIO/gpio.h \
+  ../APP/../HAL/PIR/../../MCAL/GPIO/../../LIB/std_types.h \
+  ../APP/../HAL/PIR/../../LIB/std_types.h ../APP/../HAL/MOTOR/motor.h \
+  ../APP/../HAL/MOTOR/../../LIB/std_types.h ../APP/../MCAL/UART/uart.h \
+  ../APP/../MCAL/UART/../../LIB/std_types.h ../APP/../LIB/std_types.h \
+  ../APP/../HAL/LCD/lcd.h ../APP/../HAL/LCD/../../LIB/std_types.h \
+  ../APP/../HAL/3\ Leds/leds.h \
+  ../APP/../HAL/3\ Leds/../../MCAL/GPIO/gpio.h \
+  ../APP/../HAL/Ultrasonic/ultrasonic_sensor.h \
+  ../APP/../HAL/Ultrasonic/../../MCAL/ICU/icu.h \
+  ../APP/../HAL/Ultrasonic/../../MCAL/ICU/../../LIB/common_macros.h \
+  ../APP/../HAL/Ultrasonic/../../MCAL/ICU/../../LIB/std_types.h \
+  ../APP/../HAL/Ultrasonic/../../LIB/std_types.h \
+  ../APP/../HAL/Ultrasonic/../../MCAL/GPIO/gpio.h \
+  ../APP/../MCAL/EXT_INT/EXT_INT.h \
+  ../APP/../MCAL/EXT_INT/../../LIB/std_types.h
+
+../APP/Application.h:
+
+../APP/../HAL/PIR/pir.h:
+
+../APP/../HAL/PIR/../../MCAL/GPIO/gpio.h:
+
+../APP/../HAL/PIR/../../MCAL/GPIO/../../LIB/std_types.h:
+
+../APP/../HAL/PIR/../../LIB/std_types.h:
+
+../APP/../HAL/MOTOR/motor.h:
+
+../APP/../HAL/MOTOR/../../LIB/std_types.h:
+
+../APP/../MCAL/UART/uart.h:
+
+../APP/../MCAL/UART/../../LIB/std_types.h:
+
+../APP/../LIB/std_types.h:
+
+../APP/../HAL/LCD/lcd.h:
+
+../APP/../HAL/LCD/../../LIB/std_types.h:
+
+../APP/../HAL/3\ Leds/leds.h:
+
+../APP/../HAL/3\ Leds/../../MCAL/GPIO/gpio.h:
+
+../APP/../HAL/Ultrasonic/ultrasonic_sensor.h:
+
+../APP/../HAL/Ultrasonic/../../MCAL/ICU/icu.h:
+
+../APP/../HAL/Ultrasonic/../../MCAL/ICU/../../LIB/common_macros.h:
+
+../APP/../HAL/Ultrasonic/../../MCAL/ICU/../../LIB/std_types.h:
+
+../APP/../HAL/Ultrasonic/../../LIB/std_types.h:
+
+../APP/../HAL/Ultrasonic/../../MCAL/GPIO/gpio.h:
+
+../APP/../MCAL/EXT_INT/EXT_INT.h:
+
+../APP/../MCAL/EXT_INT/../../LIB/std_types.h:
---
+++ AVR ATmega32/AVR_ATmega32/Debug/HAL/Ultrasonic/ultrasonic_sensor.d
@@ -0,0 +1,23 @@
+HAL/Ultrasonic/ultrasonic_sensor.o HAL/Ultrasonic/ultrasonic_sensor.o:  \
+ ../HAL/Ultrasonic/ultrasonic_sensor.c \
+  ../HAL/Ultrasonic/ultrasonic_sensor.h \
+  ../HAL/Ultrasonic/../../MCAL/ICU/icu.h \
+  ../HAL/Ultrasonic/../../MCAL/ICU/../../LIB/common_macros.h \
+  ../HAL/Ultrasonic/../../MCAL/ICU/../../LIB/std_types.h \
+  ../HAL/Ultrasonic/../../LIB/std_types.h \
+  ../HAL/Ultrasonic/../../MCAL/GPIO/gpio.h \
+  ../HAL/Ultrasonic/../../MCAL/GPIO/../../LIB/std_types.h
+
+../HAL/Ultrasonic/ultrasonic_sensor.h:
+
+../HAL/Ultrasonic/../../MCAL/ICU/icu.h:
+
+../HAL/Ultrasonic/../../MCAL/ICU/../../LIB/common_macros.h:
+
+../HAL/Ultrasonic/../../MCAL/ICU/../../LIB/std_types.h:
+
+../HAL/Ultrasonic/../../LIB/std_types.h:
+
+../HAL/Ultrasonic/../../MCAL/GPIO/gpio.h:
+
+../HAL/Ultrasonic/../../MCAL/GPIO/../../LIB/std_types.h:
